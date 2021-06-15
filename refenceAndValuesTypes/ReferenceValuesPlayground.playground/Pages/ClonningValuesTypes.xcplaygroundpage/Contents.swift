1941833575e00d380337bbde22b5ecd1c0c4c237
Jeann Luiz <jeann.chuab@gmail.com>
2021-06-15 11:52:05 -0300

    Updating playground

--- refenceAndValuesTypes/ReferenceValuesPlayground.playground/Pages/ClonningValuesTypes.xcplaygroundpage/Contents.swift
+++ refenceAndValuesTypes/ReferenceValuesPlayground.playground/Pages/ClonningValuesTypes.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ struct NameStruct {
     var lastName: String
 }
 
-class Name {
+class Pai {
     var firstName: String
     var lastName: String
     var degree: String
@@ -17,56 +17,71 @@ class Name {
         self.degree = degree
     }
     
-    func clone() -> Name {
-        let name = Name(firstName: self.firstName, lastName: self.lastName, degree: self.degree)
+    func clone() -> Pai {
+        let name = Pai(firstName: self.firstName, lastName: self.lastName, degree: self.degree)
         return name
     }
-//    convenience init(firstName: String) {
-//        self.firstName = firstName
-//        lastName = ""
-//        degree = ""
-        
-//        self.init(firstName: firstName, lastName: lastName, degree: degree)
-//    }
+    
+    func exibir() {
+        print("exibir class Name")
+    }
 }
 
-class NameWar: Name {
+class Filha: Pai {
     var warname: String
     
     init(warname: String) {
         self.warname = warname
         super.init(firstName: "", lastName: "", degree: "")
     }
+    
+    override func exibir() {
+        print("exibir class NameWar")
+    }
+    
+    func metodoClassFilha() {
+        
+    }
 }
 
-var puta: NameWar = NameWar(warname: "Samanta")
+class Tools {
+    static func imprimeNome(param: Pai)  {
+        print(param.firstName)
+    }
+}
+
+var varPai: Pai = Pai(firstName: "Jose", lastName: "Luiz", degree: "")
+var varFilha: Filha = Filha(warname: "Samanta")
+
+Tools.imprimeNome(param: varFilha)
+
 //puta.
 
-var lanesName: Name = Name(firstName: "Kell", lastName: "Lanes", degree: "Bachelor's")
-print(lanesName)
-
-var claraName = lanesName
-//claraName.firstName = "Clara"
-print(claraName.firstName)
-print(lanesName.firstName)
-
-
-
-//struct
-var jeannName: NameStruct = NameStruct(firstName: "Jeann", lastName: "Chuab")
-
-var joseName = jeannName
-var roseName = jeannName
-
-print(joseName.firstName)
-print(roseName.firstName)
-
-joseName.firstName = "Jose"
-roseName.firstName = "Rose"
-
-print(jeannName.firstName)
-print(joseName.firstName)
-print(roseName.firstName)
+//var lanesName: Name = Name(firstName: "Kell", lastName: "Lanes", degree: "Bachelor's")
+//print(lanesName)
+//
+//var claraName = lanesName
+////claraName.firstName = "Clara"
+//print(claraName.firstName)
+//print(lanesName.firstName)
+//
+//
+//
+////struct
+//var jeannName: NameStruct = NameStruct(firstName: "Jeann", lastName: "Chuab")
+//
+//var joseName = jeannName
+//var roseName = jeannName
+//
+//print(joseName.firstName)
+//print(roseName.firstName)
+//
+//joseName.firstName = "Jose"
+//roseName.firstName = "Rose"
+//
+//print(jeannName.firstName)
+//print(joseName.firstName)
+//print(roseName.firstName)
 
 
 
